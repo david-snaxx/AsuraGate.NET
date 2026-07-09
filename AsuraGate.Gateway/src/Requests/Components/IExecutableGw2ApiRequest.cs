895e10bc069914e31e3274a3ea3cb317518da170
DavidSnaxx <david.snaxx@proton.me>
2026-07-09 04:38:58 -0400

    added IExecutableGw2ApiRequest wrapper to seperate a stateful request from an endpoint representation object

---
+++ AsuraGate.Gateway/src/Requests/Components/IExecutableGw2ApiRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace AsuraGate.Gateway.Requests.Components;
+
+public interface IExecutableGw2ApiRequest<TModel>
+{
+    IGw2ApiRequest<TModel> BaseRequest { get; }
+    IEnumerable<KeyValuePair<string, string>> ExtraQueryParameters { get; }
+}
+
+/// <summary>
+/// Represents a request directed to the GW2 API with additional query parameters.
+/// </summary>
+/// <typeparam name="TModel">The GW2 API model type and or ID type returned by this endpoint.</typeparam>
+internal class ExecutableGw2ApiRequest<TModel> : IExecutableGw2ApiRequest<TModel>
+{
+    public IGw2ApiRequest<TModel> BaseRequest { get; set; }
+    public IEnumerable<KeyValuePair<string, string>> ExtraQueryParameters { get; set; }
+}
---
+++ AsuraGate.Gateway/src/Requests/Components/RequestMixins.cs
@@ -0,0 +1,19 @@
+﻿namespace AsuraGate.Gateway.Requests.Components;
+
+public static class ClientMixins
+{
+    public static IExecutableGw2ApiRequest<TModel> GetById<TModel, TId>(
+        this IGetsSingle<TModel, TId> request,
+        TId id,
+        CancellationToken ct = default)
+    {
+        if (id == null) throw new ArgumentNullException(nameof(id));
+        return new ExecutableGw2ApiRequest<TModel>
+        {
+            BaseRequest = request, 
+            ExtraQueryParameters = [new KeyValuePair<string, string>("id", id.ToString()!)]
+        };
+    }
+    
+    public static IExecutableGw2ApiRequest<IEnumerable<TModel>> GetBulk<TModel>()
+}
