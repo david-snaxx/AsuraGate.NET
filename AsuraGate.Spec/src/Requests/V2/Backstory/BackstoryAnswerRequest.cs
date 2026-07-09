a406051be46a6c839c47843a1e4421fda7e4ba99
DavidSnaxx <david.snaxx@proton.me>
2026-07-09 06:04:45 -0400

    added v2/backstory endpoint request blueprints

---
+++ AsuraGate.Spec/src/Requests/V2/Backstory/BackstoryAnswerRequest.cs
@@ -0,0 +1,16 @@
+﻿using AsuraGate.Spec.Consts;
+using AsuraGate.Spec.Models.V2;
+using AsuraGate.Spec.Requests.Components;
+
+namespace AsuraGate.Spec.Requests.V2.Backstory;
+
+public class BackstoryAnswerRequest :
+    IGetsSingle<BackstoryAnswer, string>,
+    IGetsBulk<BackstoryAnswer, string>,
+    IGetsAll<BackstoryAnswer>,
+    IGetsIds<string>
+{
+    public string EndpointUrl { get; } = Gw2ApiEndpointUrl.BackstoryAnswer;
+    public bool IsAuthenticated { get; } = false;
+    public bool IsLocalized { get; } = false;
+}
---
+++ AsuraGate.Spec/src/Requests/V2/Backstory/BackstoryQuestionRequest.cs
@@ -0,0 +1,16 @@
+﻿using AsuraGate.Spec.Consts;
+using AsuraGate.Spec.Models.V2;
+using AsuraGate.Spec.Requests.Components;
+
+namespace AsuraGate.Spec.Requests.V2.Backstory;
+
+public class BackstoryQuestionRequest :
+    IGetsSingle<BackstoryQuestion, int>,
+    IGetsBulk<BackstoryQuestion, int>,
+    IGetsAll<BackstoryQuestion>,
+    IGetsIds<int>
+{
+    public string EndpointUrl { get; } = Gw2ApiEndpointUrl.BackstoryQuestion;
+    public bool IsAuthenticated { get; } = false;
+    public bool IsLocalized { get; } = false;
+}
